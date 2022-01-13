0290143ee3372efa16bf50a559f4716832b22041
kimhsr <kimhsr@naver.com>
2022-01-13 16:13:58 +0900

    commit_2

---
+++ 2_변수.py
@@ -0,0 +1,7 @@
+foo = "Hello"
+print(foo)
+# 12foo = "Hello" # 변수 이름 앞에 숫자 오면 안됨
+foo12 = "Hello" # 변수 이름 뒤에 숫자는 됨
+변수 = "Hello" # 파이썬은 한글로도 변수가 가능
+# ab cd = "Hello" # 변수 이름에 띄어쓰기 들어가면 안됨
+ab_cd = "Hello" # 띄어쓰기 대신 _ 사용
