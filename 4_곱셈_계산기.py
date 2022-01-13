e2dbe1841dc0599f7dcc1fe2ca518610bedb393a
kimhsr <kimhsr@naver.com>
2022-01-13 16:50:54 +0900

    commit_2

---
+++ 4_곱셈_계산기.py
@@ -0,0 +1,3 @@
+num1 = int(input("첫번째 숫자 입력 >> ")) #int() : 문자열 -> 정수형
+num2 = int(input("두번째 숫자 입력 >> ")) #int() : 문자열 -> 정수형
+print("곱셈 결과는 {}입니다.".format(num1 * num2))
---
+++ 7_break_continue.py
@@ -0,0 +1,6 @@
+num = 0
+while True: # 무한 루프
+   num += 1
+   print(num)
+   if num == 20: # == : 양쪽 값이 같냐?
+       break # 반복문을 강제 탈출시킴
