1add97353df21310c48697d53ec753ff556ea54b
kimhsr <kimhsr@naver.com>
2022-01-13 16:58:28 +0900

    commit_3

--- 7_break_continue.py
+++ 7_break_continue.py
@@ -1,6 +1,8 @@
 num = 0
 while True: # 무한 루프
    num += 1
+   if num % 2 == 1: # 만약 num이 홀수라면?
+       continue # 반복문의 조건부 문장으로 돌려보냄
    print(num)
    if num == 20: # == : 양쪽 값이 같냐?
        break # 반복문을 강제 탈출시킴
